bd8210108bb3a4a092e879dfefde18fb91afde70
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-01-25 23:46:25 +0900

    add npc_pool.erl to manage npcs. TODO: add npc script handler (process each npc.).

---
+++ src/npc_pool.erl
@@ -0,0 +1,43 @@
+%%
+%% npc_pool.erl
+%%
+%%
+
+-module(npc_pool).
+-include_lib("amqp_client.hrl").
+
+-export([start_link/3]).
+-export([terminate/2]).
+-export([init/1]).
+-export([handle_info/2]).
+
+start_link(ServerIp, ToClientEx, FromClientEx) ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [ServerIp, ToClientEx, FromClientEx], []).
+
+init(Args) ->
+    [ServerIp, ToClientEx, FromClientEx] = Args,
+	{ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"move.#">>])}.
+
+%%
+%% APIs
+%%
+
+%% gen_server behaviour %%        
+terminate(_Reason, State) ->
+	bidir_mq:shutdown_by_state(State),
+	ok.
+
+%% just after setup, this message will arrive.
+handle_info(#'basic.consume_ok'{}, State) ->
+	{noreply, State};
+
+%% while subscribing, message will be delivered by #amqp_msg
+handle_info( {#'basic.deliver'{routing_key = _RoutingKey}, #amqp_msg{payload = Body}} , State) ->
+	{_ServerIp, ToClientEx, _FromClientEx, {_Connection, ChTC, _ChFC}} = State,
+	BinMsg = <<"info: Hello, this is move_srv!">>,
+	amqp_channel:cast(ChTC,
+		#'basic.publish'{exchange = ToClientEx, routing_key = <<"move.id.99999">> },
+		#amqp_msg{payload = BinMsg}),
+	{noreply, State}.
+
+
