0b501c3805e92eacea13efca85aab34d41474417
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-02-11 00:52:39 +0900

    Fixed: now, works. just works - npc_pool:new/0.

--- src/npc_pool.erl
+++ src/npc_pool.erl
@@ -14,8 +14,7 @@
 -export([new/1]).
 -export([add/2]).
 -export([lookup/1]).
-
--compile(export_all).
+-export([reset/0]).
 
 %%
 %% APIs
@@ -33,6 +32,36 @@ add(K, V) ->
 lookup(K) ->
 	Reply = gen_server:call(?MODULE, {lookup, K}).
 
+reset() ->
+	Reply = gen_server:call(?MODULE, {set_counter, 0}).
+
+%%
+%% Utilities
+%%
+
+get_counter_value(Pid) ->
+	MyBucket = <<"counter">>,
+	BinK = erlang:list_to_binary("npc"),
+	riakc_pb_socket:get(Pid, MyBucket, BinK).
+
+make_next_value({error,notfound}) ->
+	MyBucket = <<"counter">>,
+	BinK = erlang:list_to_binary("npc"),
+	Obj = riakc_obj:new(MyBucket, BinK, 1),
+	{rc, 1, Obj};
+
+make_next_value({ok,Fetched}) ->
+	Val = binary_to_term(riakc_obj:get_value(Fetched)),
+	Newval = Val + 1,
+	UpdatedObj = riakc_obj:update_value(Fetched, Newval),
+	{rc, Newval, UpdatedObj}.
+
+get_next_id(Pid) ->
+	{rc, Count, Obj} = make_next_value(get_counter_value(Pid)),
+	{ok, NewestObj1} = riakc_pb_socket:put(Pid, Obj, [return_body]),
+	Count.
+
+
 %%
 %% Behaviors
 %%
@@ -48,18 +77,27 @@ init(Args) ->
 terminate(_Reason, State) ->
 	ok.
 
+handle_call({set_counter, V}, From, State) ->
+	{Pid, Npcs} = State,
+	MyBucket = <<"counter">>,
+	BinK = erlang:list_to_binary("npc"),
+	Obj = riakc_obj:new(MyBucket, BinK, V),
+	{ok, NewestObj1} = riakc_pb_socket:put(Pid, Obj, [return_body]),
+	Val2 = binary_to_term(riakc_obj:get_value(NewestObj1)),
+	{reply, {ok, Val2}, State};
+
 handle_call({new, Type}, From, State) ->
 	{Pid, Npcs} = State,
-	MyBucket = <<"test">>,
-	BinK = erlang:list_to_binary(K),
-	Obj1 = riakc_obj:new(MyBucket, BinK, V),
+	MyBucket = <<"npc">>,
+	Id = get_next_id(Pid),
+	BinK = erlang:term_to_binary(Id),
+	Obj1 = riakc_obj:new(MyBucket, BinK, "hoge"),
 	riakc_pb_socket:put(Pid, Obj1),
-	{reply, ok, State};
-
+	{reply, {ok, Id}, State};
 
 handle_call({add, K, V}, From, State) ->
 	{Pid, Npcs} = State,
-	MyBucket = <<"test">>,
+	MyBucket = <<"npc">>,
 	BinK = erlang:list_to_binary(K),
 	Obj1 = riakc_obj:new(MyBucket, BinK, V),
 	riakc_pb_socket:put(Pid, Obj1),
@@ -67,7 +105,7 @@ handle_call({add, K, V}, From, State) ->
 
 handle_call({lookup, K}, From, State) ->
 	{Pid, Npcs} = State,
-	MyBucket = <<"test">>,
+	MyBucket = <<"npc">>,
 	BinK = erlang:list_to_binary(K),
 	{ok, Fetched1} = riakc_pb_socket:get(Pid, MyBucket, BinK),
 	Val1 = binary_to_term(riakc_obj:get_value(Fetched1)),
