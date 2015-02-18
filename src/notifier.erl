96d79af63b3ef11cfe91eea7a5d9f89def29b1b7
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-02-18 09:19:29 +0900

    wrote notifier:test/0. notifier mechanism works.

--- src/notifier.erl
+++ src/notifier.erl
@@ -11,10 +11,19 @@
 -export([handle_call/3]).
 
 -export([add/2]).
+-export([test/0]).
+-export([test/2]).
 
 %%
 %% APIs
 %%
+test() ->
+	io:format("request to run test/2 with 0, 1~n", []),
+	notifier:add(1000, {mfa, notifier, test, [0,1]}).
+
+test(A,B) ->
+	io:format("notifier:test/2 called. ~p, ~p ~n", [A, B]).
+
 add(DeltaMs, {mfa, M, F, A}) ->
 	Reply = gen_server:call(?MODULE, {add, DeltaMs, {mfa, M, F, A}}).
 
