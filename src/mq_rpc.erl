cb8dc82f6f5cef7a5cbc69525916c8b7ce711aa3
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-04-03 00:14:11 +0900

    write sample RPC server/client implementation of C# as comments.

--- src/mq_rpc.erl
+++ src/mq_rpc.erl
@@ -19,6 +19,53 @@ init(Args) ->
 	%% {ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"chat.#">>, <<"move.#">>])}.
 	{ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"#">>])}.
 
+
+
+
+% 
+% class RPCClient
+% {
+% 	private IConnection connection;
+% 	private IModel channel;
+% 	private string replyQueueName;
+% 	private QueueingBasicConsumer consumer;
+% 	public RPCClient()
+% 	{
+% 		var factory = new ConnectionFactory() { HostName = "localhost" };
+% 		connection = factory.CreateConnection();
+% 		channel = connection.CreateModel();
+% 		replyQueueName = channel.QueueDeclare();
+% 		consumer = new QueueingBasicConsumer(channel);
+% 		channel.BasicConsume(replyQueueName, true, consumer);
+% 	}
+% 	public string Call(string message)
+% 	{
+% 		var corrId = Guid.NewGuid().ToString();
+% 		var props = channel.CreateBasicProperties();
+% 		props.ReplyTo = replyQueueName;
+% 		props.CorrelationId = corrId;
+% 		var messageBytes = Encoding.UTF8.GetBytes(message);
+% 		channel.BasicPublish("", "rpc_queue", props, messageBytes);
+% 		while (true)
+% 		{
+% 			var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+% 			if (ea.BasicProperties.CorrelationId == corrId)
+% 			{
+% 				return Encoding.UTF8.GetString(ea.Body);
+% 			}
+% 		}
+% 	}
+% 	public void Close()
+% 	{
+% 		connection.Close();
+% 	}
+% }
+% 
+
+
+
+
+
 %%
 %% APIs
 %%
--- src/mq_rpc_srv.erl
+++ src/mq_rpc_srv.erl
@@ -20,12 +20,85 @@ start_link(ServerIp, ListenEx) ->
 
 init(Args) ->
 	[ServerIp, ListenEx] = Args,
+
 	%% {ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"chat.#">>, <<"move.#">>])}.
+	
+	%%{Connection, ChTC, ChFC} = setup_topic(ServerIp, ToClientEx, FromClientEx, ReceiveTopicList),
+	%%{ServerIp, ToClientEx, FromClientEx, {Connection, ChTC, ChFC}}.
+
+    %%%{ok, Connection} = amqp_connection:start(#amqp_params_network{host = ServerIp}),
+	%%ChTC = setup_emit_topics(Connection, ToClientEx),
+	%%ChFC = setup_receive_topics(Connection, FromClientEx, ReceiveTopicList),
+	%%{Connection, ChTC, ChFC}.
+
+%%setup_receive_topics(Connection, Exchange, TopicList) ->
+%%    {ok, Ch} = amqp_connection:open_channel(Connection),
+%%    amqp_channel:call(Ch, #'exchange.declare'{exchange = Exchange, type = <<"topic">>, auto_delete = true}),
+%%	#'queue.declare_ok'{queue = Queue} =
+%%	amqp_channel:call(Ch, #'queue.declare'{exclusive = true}),
+%%	[amqp_channel:call(Ch, #'queue.bind'{
+%%		exchange = Exchange,
+%%		routing_key = BindingKey,
+%%		queue = Queue})
+%%	|| BindingKey <- TopicList],
+%%	amqp_channel:subscribe(Ch, #'basic.consume'{queue = Queue, no_ack = true}, self()),
+%%	Ch.
+%%
+%%
 	
 	%% Setup connection
 	%%
 
-	{ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"#">>])}.
+	%%{ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"#">>])}.
+
+% 
+% // RPC Server code
+% var factory = new ConnectionFactory() { HostName = "localhost" };
+% using (var connection = factory.CreateConnection())
+% {
+% 	using (var channel = connection.CreateModel())
+% 	{
+% 		channel.QueueDeclare("rpc_queue", false, false, false, null);
+% 		channel.BasicQos(0, 1, false);
+% 		var consumer = new QueueingBasicConsumer(channel);
+% 		channel.BasicConsume("rpc_queue", false, consumer);
+% 		Console.WriteLine(" [x] Awaiting RPC requests");
+% 		while (true)
+% 		{
+% 			string response = null;
+% 			var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+% 			var body = ea.Body;
+% 			var props = ea.BasicProperties;
+% 			var replyProps = channel.CreateBasicProperties();
+% 			replyProps.CorrelationId = props.CorrelationId;
+% 			try
+% 			{
+% 				var message = Encoding.UTF8.GetString(body);
+% 				int n = int.Parse(message);
+% 				Console.WriteLine(" [.] fib({0})", message);
+% 				response = fib(n).ToString();
+% 			}
+% 			catch (Exception e)
+% 			{
+% 				Console.WriteLine(" [.] " + e.Message);
+% 				response = "";
+% 			}
+% 			finally
+% 			{
+% 				var responseBytes =
+% 				Encoding.UTF8.GetBytes(response);
+% 				channel.BasicPublish("", props.ReplyTo, replyProps,
+% 				responseBytes);
+% 				channel.BasicAck(ea.DeliveryTag, false);
+% 			}
+% 		}
+% 	}
+% }
+% 
+
+
+
+	{ok, 0}.
 
 terminate(_Reason, State) ->
 	bidir_mq:shutdown_by_state(State),
