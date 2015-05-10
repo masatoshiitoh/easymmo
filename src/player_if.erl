6a9d5edf2c9173f038b31d28c671e69c224db716
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-05-11 00:49:26 +0900

    working commit.

--- src/player_if.erl
+++ src/player_if.erl
@@ -14,7 +14,6 @@
 -export([handle_info/2]).
 -export([handle_call/3]).
 -export([ctest/0]).
--export([ctest2/0]).
 
 -export([rpc_echo/1]).
 -export([rpc_make_new_account/2]).
@@ -28,22 +27,26 @@ ctest() ->
     {ok, Connection} = amqp_connection:start(#amqp_params_network{host = "192.168.56.21"}),
 	io:format("connection ok~n",[]),
 
-	Pid = amqp_rpc_client:start(Connection, <<"echo">>),
-	io:format("player_if/ctest:start link ok, ~p~n",[Pid]),
-	io:format("player_if/ctest:call returned with '~p'~n",[ amqp_rpc_client:call(Pid, term_to_binary([<<"ctest calls!">>])) ]),
-	amqp_rpc_client:stop(Pid),
+	EchoPid = amqp_rpc_client:start(Connection, <<"echo">>),
+	io:format("player_if/ctest:call echo returned with '~p'~n",
+		[ binary_to_term(amqp_rpc_client:call(EchoPid,
+					term_to_binary( [<<"ctest calls!">>])))]),
+
+	NewAcctPid = amqp_rpc_client:start(Connection, <<"new_account">>),
+	io:format("player_if/ctest:call newaccount returned with '~p'~n",
+		[ binary_to_term(amqp_rpc_client:call(NewAcctPid,
+					term_to_binary( [<<"mailaddr@example.jp">>, <<"pasuwado">>]))) ]),
+
+	LogoutPid = amqp_rpc_client:start(Connection, <<"logout">>),
+	io:format("player_if/ctest:call logout returned with '~p'~n",
+		[ binary_to_term(amqp_rpc_client:call(LogoutPid,
+					term_to_binary([<<"sampletoken00">>]))) ]),
+
+	amqp_rpc_client:stop(LogoutPid),
+	amqp_rpc_client:stop(NewAcctPid),
+	amqp_rpc_client:stop(EchoPid),
 
 	ok.
-
-ctest2() ->
-	call_rpc(<<"echo">>, [<<"hello!">>]).
-
-call_rpc(Function, Args) when is_binary(Function), is_list(Args) ->
-    {ok, Connection} = amqp_connection:start(#amqp_params_network{host = "192.168.56.21"}),
-	Pid = amqp_rpc_client:start(Connection, Function),
-	Result = amqp_rpc_client:call(Pid, term_to_binary([<<"ctest calls!">>])),
-	amqp_rpc_client:stop(Pid),
-	Result.
 
 
 %%
@@ -52,7 +55,7 @@ call_rpc(Function, Args) when is_binary(Function), is_list(Args) ->
 
 %%
 %% client to server
-%% this is for direct call test.
+%% this is for direct call test. (why test?, -> all clients access to this via AMQP.)
 %%
 new_account(Id, Password) -> impl_new_account(Id, Password).
 login(Id, Password) -> impl_login(Id, Password).
@@ -76,10 +79,14 @@ get_default_rpcs() -> [
 	{<<"offline">>,		{?MODULE, rpc_offline}}
 ].
 
-rpc_echo(_Payload) -> <<"echo!!">>.
-rpc_make_new_account(LoginId, Password) -> ng.
-rpc_login(LoginId, Password) -> ng.
-rpc_logout(Token) -> ng.
+%%
+%% server process body here.
+%%
+%%
+rpc_echo(_Payload) -> term_to_binary({ok,"echo!!"}).
+rpc_make_new_account(LoginId, Password) -> term_to_binary({ng, notoken}).
+rpc_login(LoginId, Password) -> term_to_binary({ng, notoken}).
+rpc_logout(Token) -> term_to_binary(ng).
 rpc_check_token(Token) -> ng.
 rpc_online(Token) -> ng.
 rpc_offline(Token) -> ng.
