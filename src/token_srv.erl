0bade82430706439415ae1767b176890558a20da
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-05-15 20:24:34 +0900

    working commit.

--- src/token_srv.erl
+++ src/token_srv.erl
@@ -20,10 +20,28 @@
 %%
 %% APIs
 %%
-test() -> ng.
+test() ->
+	remove_all(),
+
+	T1 = add("0123"),
+	ok = check("0123", T1),
+	ng = check("0124", T1),
+
+	T2 = add("0123", -1),
+	ng = check("0123", T2),
+	ng = check("0124", T2),
+
+	ok.
 
 add(Id) ->
 	Reply = gen_server:call(?MODULE, {add, Id}).
+	{ok, V} = Reply,
+	V.
+
+add(Id, ExpireUnixtime) ->
+	Reply = gen_server:call(?MODULE, {add, Id, ExpireUnixtime}),
+	{ok, V} = Reply,
+	V.
 
 remove_all() ->
 	Reply = gen_server:call(?MODULE, {remove_all}).
@@ -38,8 +56,13 @@ check(Id, Token) ->
 %% Utilities
 %%
 
-gen_token() -> crypto:rand_bytes(?BYTES_OF_TOKEN).
+gen_token() ->
+	#token{token = crypto:rand_bytes(?BYTES_OF_TOKEN),
+		expire_unixtime = 0}.
 
+gen_token(ExpireUnixtime) ->
+	#token{token = crypto:rand_bytes(?BYTES_OF_TOKEN),
+		expire_unixtime = ExpireUnixtime}.
 
 %%
 %% Behaviors
@@ -66,12 +89,32 @@ handle_call({add, Id}, From, State) when is_list(Id) ->
 	MetaData = riakc_obj:get_update_metadata(Obj1),
 	io:format("~p~n", [MetaData]),
 
-	MD1 = riakc_obj:set_secondary_index(MetaData, [{{integer_index, "expire_unixtime"}, [V#loc.map_id]}]),
+	MD1 = riakc_obj:set_secondary_index(MetaData, [{{integer_index, "expire_unixtime"}, [V#token.expire_unixtime]}]),
 
 	Obj2 = riakc_obj:update_metadata(Obj1, MD1),
 
 	riakc_pb_socket:put(Pid, Obj2),
-	{reply, ok, State};
+	{reply, {ok, V}, State};
+
+handle_call({add, Id, ExpireUnixtime}, From, State) when is_list(Id) ->
+	Pid = State,
+	BinId = erlang:list_to_binary(Id),
+
+	V = gen_token(ExpireUnixtime),
+
+	Obj1 = riakc_obj:new(?UTokenBucket, BinId, V),
+	io:format("~p~n", [Obj1]),
+
+	MetaData = riakc_obj:get_update_metadata(Obj1),
+	io:format("~p~n", [MetaData]),
+
+	MD1 = riakc_obj:set_secondary_index(MetaData, [{{integer_index, "expire_unixtime"}, [V#token.expire_unixtime]}]),
+
+	Obj2 = riakc_obj:update_metadata(Obj1, MD1),
+
+	riakc_pb_socket:put(Pid, Obj2),
+	{reply, {ok, V}, State};
+
 
 handle_call({remove_all}, From, State) ->
 	Pid = State,
@@ -116,8 +159,6 @@ impl_lookup_with_integer(Pid, Attr, Key) ->
 	{ok, {index_results_v1, L, _,_}} = riakc_pb_socket:get_index_eq(Pid, ?UTokenBucket,{integer_index, Attr}, Key),
 	L.
 
-
-
 impl_check_is_online(Pid, Id, Token) when is_list(Id) ->
 	BinId = erlang:list_to_binary(Id),
 	impl_check_is_online(Pid, BinId, Token);
@@ -127,7 +168,6 @@ impl_check_is_online(Pid, BinId, Token) when is_binary(BinId) ->
 		{ok, Fetched1} -> true;
 		{error, notfound} -> false
 	end.
-
 
 impl_online(Pid, Id, Token) when is_list(Id) ->
 	BinId = erlang:list_to_binary(Id),
@@ -151,7 +191,5 @@ impl_offline(Pid, Id, Token) when is_list(Id) ->
 
 impl_offline(Pid, BinId, Token) when is_binary(BinId) ->
 	0.
-
-
 
 
