c4ca704e7762b2179d5f0f686c74ba1ec7e92589
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-05-26 09:17:06 +0900

    Changed: now, pc_simple will not be registerd.

--- src/pc_simple.erl
+++ src/pc_simple.erl
@@ -1,19 +1,23 @@
 -module(pc_simple).
 -behaviour(gen_fsm).
 
--export([start_link/1, stop/0]).
--export([attack/1, get_state/0]).
+-export([start_link/1, stop/1]).
+-export([attack/2, get_state/1]).
+
 -export([init/1, good/2, dead/2]).
 -export([handle_event/3, terminate/3]).
 -export([handle_sync_event/4, handle_info/3, code_change/4]).
 
-start_link(Hp) -> gen_fsm:start_link({local, pc_simple}, pc_simple, Hp, []).
+%%
+%% FSM will not be registerd.
+%%
+start_link(Hp) -> gen_fsm:start_link(pc_simple, Hp, []).
 
-stop() -> gen_fsm:send_all_state_event(pc_simple, stop).
+stop(Pid) -> gen_fsm:send_all_state_event(Pid, stop).
 
-attack(Damage) -> gen_fsm:send_event(pc_simple, {attack, Damage}).
+attack(Pid, Damage) -> gen_fsm:send_event(Pid, {attack, Damage}).
 
-get_state() -> gen_fsm:sync_send_all_state_event(pc_simple, get_state).
+get_state(Pid) -> gen_fsm:sync_send_all_state_event(Pid, get_state).
 
 init(StartHp) -> {ok, good, {stat, StartHp}}.
 
@@ -22,7 +26,7 @@ good({attack, Damage}, {stat, Hp}) ->
 	if
 		NewHp < 1  ->
 			io:fwrite("uncon!!~n"),
-            {next_state, dead, {stat, NewHp}, 3000};
+            {next_state, dead, {stat, NewHp}, 10000};
 
         true ->
             {next_state, good, {stat, NewHp}}
