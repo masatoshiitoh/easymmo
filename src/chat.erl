b546ebb07bf3a57ffc5cb25c562654dc8802f05b
Masatoshi Itoh <masatoshi9953@gmail.com>
2015-06-06 18:57:24 +0900

    working commit (build fails).

---
+++ src/chat.erl
@@ -0,0 +1,60 @@
+%%
+%%
+
+-module(chat).
+-include_lib("amqp_client.hrl").
+
+-export([connect/1]).
+-export([disconnect/1]).
+
+%%
+%% APIs
+%%
+connect(Id) ->
+	Reply = gen_server:call(?MODULE, {connect, Id}).
+
+disconnect(Id) ->
+	Reply = gen_server:call(?MODULE, {disconnect, Id}).
+
+broadcast(Id, Payload) ->
+	Reply = gen_server:call(?MODULE, {broadcast, Id, Payload}).
+
+%%
+%% Behaviors
+%%
+start_link(ServerIp, ToClientEx, FromClientEx) ->
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [ServerIp, ToClientEx, FromClientEx], []).
+
+init(Args) ->
+	[ServerIp, ToClientEx, FromClientEx] = Args,
+	{ok, bidir_mq:init_topic(ServerIp, ToClientEx, FromClientEx, [<<"chat.#">>])}.
+
+terminate(_Reason, State) ->
+	bidir_mq:shutdown_by_state(State),
+	ok.
+
+%% just after setup, this message will arrive.
+handle_info(#'basic.consume_ok'{}, State) ->
+	{noreply, State};
+
+%% while subscribing, message will be delivered by #amqp_msg
+handle_info( {#'basic.deliver'{routing_key = _RoutingKey}, #amqp_msg{payload = Body}} , State) ->
+	{_ServerIp, ToClientEx, _FromClientEx, {_Connection, ChTC, _ChFC}} = State,
+	Message = Body ,
+	amqp_channel:cast(ChTC,
+		#'basic.publish'{exchange = ToClientEx, routing_key = <<"chat.open">> },
+		#amqp_msg{payload = Message}),
+	{noreply, State}.
+
+handle_call({broadcast, Id, Payload}, From, State) ->
+	{_ServerIp, ToClientEx, FromClientEx, {_Connection, ChTC, _ChFC}} = State,
+	BinMsg = jsx:encode([{<<"id">>, list_to_binary(Id)}, {<<"message">>, list_to_binary(Payload)}]),
+	%%BinMsg = list_to_binary(JsonMsg) ,
+	amqp_channel:cast(ChTC,
+		#'basic.publish'{exchange = FromClientEx, routing_key = <<"chat.open">> },
+		#amqp_msg{payload = BinMsg}),
+	{reply, ok, State}.
+
+
+
+
